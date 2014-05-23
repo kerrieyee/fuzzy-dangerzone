f2c44d5c3290335b8d97d951128d70ecc36a1507
Kerrie Yee <kerrie.yee@gmail.com>
2014-05-23 14:01:50 -0700

    api for dictionary/Markov

--- dictionary.rb
+++ dictionary.rb
@@ -1,2 +1,43 @@
 class Dictionary
+  def initialize(text)
+    @text = text
+  end
+
+  def digest(text)
+    #this will create hashes
+    #this digest will be in a bst
+  end
+
+  def build_text(n_words)
+  end
+
+  private
+
+  def search
+    #if we don't find it we insert_node
+    #if we do find it node.increment, node.insert
+  end
+  #Markov Patterns
+
+  def insert_node(word)
+    Node.new(word)
+  end
+
+
+end
+
+class Node
+  def initialize(word)
+    @count = 0
+    @following_array = []
+    @value = word
+  end
+
+  def increment
+    @count += 1
+  end
+
+  def insert_into_following_array(word)
+    @following_array << word
+  end
 end
